87ce6dc8ac2617ca9ecc9fe2f564a8abf5e35c6f
purplefeetguy@hotmail.com <purplefeetguy@hotmail.com>
2024-12-29 00:46:56 -1000

    terraform init successful

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -60,6 +60,7 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 }
 
 # Output the resource group name
